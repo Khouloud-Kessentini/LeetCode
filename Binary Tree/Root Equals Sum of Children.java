a27f4640a9d70c3268a7d7dd831f3b7c49195cc9
khouloud-kessentini <85420156+Khouloud-Kessentini@users.noreply.github.com>
2023-12-14 16:19:41 +0100

    Create Root Equals Sum of Children.java

---
+++ Binary Tree/Root Equals Sum of Children.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        return root.val == left.val + right.val? true: false;
+    }
+}
