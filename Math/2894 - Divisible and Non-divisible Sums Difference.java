465ea1e9ee81323ed0e0b764389b08c03b93fe95
khouloud-kessentini <85420156+Khouloud-Kessentini@users.noreply.github.com>
2024-01-16 13:13:48 +0100

    Create 	2894 - Divisible and Non-divisible Sums Difference.java

---
+++ Math/2894 - Divisible and Non-divisible Sums Difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int i, sum1=0 , sum2=0;
+        for(i=1;i<n+1;i++){
+            if(i%m!=0){
+                sum1+=i;
+            }
+            else{
+                sum2+=i;
+            }
+        }
+        return sum1 - sum2;
+    }
+}
