3c7c63625f5266f37eb21697c55b8244fe2b2925
khouloud-kessentini <85420156+Khouloud-Kessentini@users.noreply.github.com>
2024-02-11 21:33:13 +0100

    Create 1 - Two Sum.py

---
+++ Hash Table/1 - Two Sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashMap = {}
+        for i, n in enumerate(nums):
+            difference = target - n
+            if difference in hashMap:
+                return [hashMap[difference], i]
+            hashMap[n] =i
+        return
+        
